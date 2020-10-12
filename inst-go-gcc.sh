591959070809c68cf93b2446b0d43248340a435a
Ubuntu <ubuntu@ip-172-31-69-247.ec2.internal>
2020-10-12 21:13:14 +0000

    update

--- inst-go-gcc.sh
+++ inst-go-gcc.sh
@@ -6,20 +6,20 @@ apt install make
 make -v
 
 ## install
-apt install build-essential
+apt install -y build-essential
 gcc -v
 
 snap install go --classic
-/*
+
 ## install go 1.13
-wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
-sha256sum go1.13.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-mkdir go
-mkdir go/{src,bin,pkg}
-export $GOPATH=$HOME/go
-*/
+#wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+#sha256sum go1.13.linux-amd64.tar.gz
+#tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
+#export PATH=$PATH:/usr/local/go/bin
+#mkdir go
+#mkdir go/{src,bin,pkg}
+#export $GOPATH=$HOME/go
+
 
 go version
 
